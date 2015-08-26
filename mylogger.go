decff54614000a329c1062935ba47e7fbcc90c37
Frank Song <spf.doudou@gmail.com>
2015-08-26 14:28:53 +0800

    http json rpc test

--- mylogger.go
+++ mylogger.go
@@ -1,8 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"github.com/franksong/logger"
+	"github.com/gorilla/rpc"
+	"github.com/gorilla/rpc/json"
+	"net/http"
 )
+
+type HelloArgs struct {
+	Who  string
+	Name string
+	Age  uint32
+}
+
+type HelloReply struct {
+	Message string
+	Namer   string
+	Ager    uint32
+}
+
+type HelloService struct{}
+
+func (h *HelloService) Say(r *http.Request, args *HelloArgs, reply *HelloReply) error {
+	reply.Message = "Hello, " + args.Who + "!"
+	reply.Namer = args.Name
+	reply.Ager = args.Age
+	logger.Error("Say:, ", args)
+	return nil
+}
 
 func main() {
 	logger.Debug("test debug!!!")
@@ -10,4 +36,19 @@ func main() {
 	logger.Warning("test warning!!!")
 	logger.Error("test error!!!")
 	// logger.Fatal("test fatal!!!")
+
+	s := rpc.NewServer()
+	s.RegisterCodec(json.NewCodec(), "application/json-rpc")
+	pServer := &HelloService{}
+	err := s.RegisterService(pServer, "Hello")
+	fmt.Println("RegisterService: ")
+	logger.Debug("RegisterService")
+	if err != nil {
+		fmt.Println("RegisterService: ", err)
+	}
+	http.Handle("/rpc", s)
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		logger.Error("listenandserve error: ", err)
+	}
 }
